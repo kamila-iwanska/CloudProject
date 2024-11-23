75003b0fde6201050551c8f4ce1287e751cea793
kamila-iwanska <253027@student.pwr.edu.pl>
2024-11-23 15:17:03 +0100

    Refactor Login and SignUp components for improved styling and structure

--- FrontEnd/src/Login.jsx
+++ FrontEnd/src/Login.jsx
@@ -1,35 +1,37 @@
 import { useState } from 'react'
 import { cognitoLogin } from "./cognito"
+import './login-style.css'
 
 export function Login({ setPage, setAccessToken }) {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        console.log({ username, password });
-        cognitoLogin(username, password)
-          .then((result) => {
-            console.log(result);
-            setAccessToken(result.AuthenticationResult.AccessToken);
-            setPage('chat');
-          })
-          .catch((error) => {
-            window.alert(error.message);
-          });
-      }}>
-        <label htmlFor="username">Username</label><br />
-        <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} /><br />
-        <label htmlFor="password">Password</label><br />
-        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} /><br />
-        <button type="submit">Log in</button>
-      </form>
-      <button onClick={() => setPage('sign-up')}>Sign up instead</button>
+    <div className ="login-page">
+      <div className ="login-form-container">
+        <h1>Login</h1>
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          console.log({ username, password });
+          cognitoLogin(username, password)
+            .then((result) => {
+              console.log(result);
+              setAccessToken(result.AuthenticationResult.AccessToken);
+              setPage('chat');
+            })
+            .catch((error) => {
+              window.alert(error.message);
+            });
+        }}>
+          <label htmlFor="username">Username</label>
+          <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} /><br />
+          <label htmlFor="password">Password</label>
+          <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} /><br />
+          <button type="submit">Log in</button>
+        </form>
+        <button className='sign-up-btn' onClick={() => setPage('sign-up')}>Sign up instead</button>
+      </div>
     </div>
-    
   )
 }
 
--- FrontEnd/src/SignUp.jsx
+++ FrontEnd/src/SignUp.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { cognitoSignUp } from "./cognito"
+import "./signup-style.css"
 
 export function SignUp({ setPage }) {
     const [username, setUsername] = useState('')
@@ -7,29 +8,31 @@ export function SignUp({ setPage }) {
     const [password, setPassword] = useState('')
 
     return (
-      <div>
-        <h1>SignUp</h1>
-        <form onSubmit={(e) => {e.preventDefault();
-        console.log({ email, username, password });
-        cognitoSignUp(email, username, password)
-          .then((result) => {
-            console.log(result);
-            window.alert("Sign up successful, please confirm your email address.");
-          })
-          .catch((error) => {
-            window.alert(error.message);
-          });
-        setPage('login')
-        }}>
-          <label htmlFor ="username">Username</label><br />
-          <input type="text" placeholder="Username" value = {username} onChange = {(e)=> setUsername(e.target.value)}/><br />
-          <label htmlFor ="email">E-mail</label><br />
-          <input type="email" placeholder="E-mail" value = {email} onChange={(e)=> setEmail(e.target.value)}/><br />
-          <label htmlFor="password">Password</label><br />
-          <input type="password" placeholder="Password" value = {password} onChange={(e) => setPassword(e.target.value)}/><br />
-          <button type="submit">SignUp</button>
-        </form>
-        <button onClick={() => setPage('login')}>Login instead</button>
+      <div className = "signup-page">
+        <div className = "signup-form-container">
+          <h1>SignUp</h1>
+          <form onSubmit={(e) => {e.preventDefault();
+          console.log({ email, username, password });
+          cognitoSignUp(email, username, password)
+            .then((result) => {
+              console.log(result);
+              window.alert("Sign up successful, please confirm your email address.");
+            })
+            .catch((error) => {
+              window.alert(error.message);
+            });
+          setPage('login')
+          }}>
+            <label htmlFor ="username">Username</label>
+            <input type="text" placeholder="Username" value = {username} onChange = {(e)=> setUsername(e.target.value)}/><br />
+            <label htmlFor ="email">E-mail</label>
+            <input type="email" placeholder="E-mail" value = {email} onChange={(e)=> setEmail(e.target.value)}/><br />
+            <label htmlFor="password">Password</label>
+            <input type="password" placeholder="Password" value = {password} onChange={(e) => setPassword(e.target.value)}/><br />
+            <button type="submit">SignUp</button>
+          </form>
+          <button className = "login-btn" onClick={() => setPage('login')}>Login instead</button>
+        </div>
       </div>
     )
   }
