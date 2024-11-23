52573622686023445cec02fc8abeccb3727d9c8a
kamila-iwanska <253027@student.pwr.edu.pl>
2024-11-23 21:37:01 +0100

    Refactor Chat component layout and styling

--- FrontEnd/src/Chat.jsx
+++ FrontEnd/src/Chat.jsx
@@ -1,52 +1,63 @@
 import { useState, useEffect } from 'react'
 import { APIget, APIpost } from './API.js'
+import "./chat-style.css"
 
-export function Chat({ setPage, accessToken}) {
+export function Chat({ setPage, accessToken }) {
   const [users, setUsers] = useState([]);
   const [selectedUserSub, setSelectedUserSub] = useState(null);
-  const[messages, setMessages] = useState([]);
-  const[newMessage, setNewMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
 
-  useEffect(() => {APIget('/users', accessToken).then((data) => {setUsers(data)})}, []); 
+  useEffect(() => { APIget('/users', accessToken).then((data) => { setUsers(data) }) }, []);
 
   const listUsers = users.map(user =>
-    <li key={user.sub} onClick={() => setSelectedUserSub(user.sub)} style={user.sub==selectedUserSub ? {color: 'red'}:null}>
+    <p className= "user-panel" key={user.sub} onClick={() => setSelectedUserSub(user.sub)} style={user.sub == selectedUserSub ? { backgroundColor: 'pink' } : null}>
       {user.username}
-    </li>
+    </p>
   );
-  
-  useEffect(() =>{
-    if (selectedUserSub !=null){
-      APIget('/messages/' + selectedUserSub, accessToken).then((data) => {setMessages(data)})
+
+  useEffect(() => {
+    if (selectedUserSub != null) {
+      APIget('/messages/' + selectedUserSub, accessToken).then((data) => { setMessages(data) })
     }
   }, [selectedUserSub]);
-    
+
   const listMessages = messages.map(message =>
-    <li key={message.id}>
-      {message.sender} <br/>
+    <p className = {message.receiver!=selectedUserSub ? "sender-message" : "receiver-message" } key={message.id}>
       {message.message}
-    </li>
+    </p>
   )
 
-  return(
+  return (
     <>
-      <div>
-        <h1>Users</h1>
-        <ul>{listUsers}</ul>
+      <div className="chat-page">
+
+        <div className="users-section">
+          <h2>Users</h2>
+          {listUsers}
+        </div>
+
+        <div className="chat-section">
+          <h2>Chat</h2>
+          <div className="messages-section">
+          {listMessages}
+          </div>
+          <div className='input-section'>
+            <input type="text" placeholder='Message' value={newMessage} onChange={(e) => setNewMessage(e.target.value)} />
+            <button disabled={selectedUserSub == null} onClick={() => {
+              APIpost('/messages', accessToken, { receiver: selectedUserSub, message: newMessage }).then((result) => {
+                setMessages([...messages, result]);
+                setNewMessage('');
+              }
+              )
+            }}>Send</button>
+          </div>
+        </div>
+
+        <div className="logout-btn">
+          <button onClick={() => setPage('login')}>Log out</button>
+        </div>
       </div>
-      <div>
-        <h1>Chat</h1>
-        <ul>{listMessages}</ul>
-      </div>
-      <input type="text" placeholder='Message' value = {newMessage} onChange={(e)=>setNewMessage(e.target.value)}/>
-      <button disabled = {selectedUserSub == null} onClick={() => {
-        APIpost('/messages', accessToken, {receiver: selectedUserSub, message: newMessage}).then((result) => {
-          setMessages([...messages, result]);
-          setNewMessage('');
-        }
-      )
-      }}>Send</button>< br/>
-      <button onClick={() => setPage('login')}>Log out</button>
     </>
-)
+  )
 }
