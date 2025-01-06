bfe2c57a00a8482d46f426344235d82db28840f9
kamila-iwanska <253027@student.pwr.edu.pl>
2025-01-06 22:10:39 +0100

    Add CloudTrail configuration with S3 bucket and IAM policy

---
+++ Terraform/cloudTrail.tf
@@ -0,0 +1,33 @@
+resource "aws_s3_bucket" "s3_bucket"{
+    bucket = "s3-bucket-free"
+    force_destroy = true
+}
+
+data "aws_iam_policy_document" "cloudtrail_policy" {
+  statement {
+    principals {
+        type        = "Service"
+        identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions = ["s3:*"]
+
+    resources = [
+        aws_s3_bucket.s3_bucket.arn, //operacja bezpośrednio na bucket
+        "${aws_s3_bucket.s3_bucket.arn}/*" //uprawnienia do konkretnych folderów w bucket
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "s3_bucket_policy" {
+    bucket = aws_s3_bucket.s3_bucket.bucket
+    policy = data.aws_iam_policy_document.cloudtrail_policy.json
+}
+
+resource "aws_cloudtrail" "cloudtrail" {
+    depends_on = [ aws_s3_bucket_policy.s3_bucket_policy ]
+
+    name = "cloudtrail"
+    s3_bucket_name = aws_s3_bucket.s3_bucket.bucket
+    s3_key_prefix = "cloudtrail"
+}
